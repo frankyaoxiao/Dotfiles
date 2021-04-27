ec308bcee7a6979099f6950fdd831a6c3ec7c861
frank <frankyaoxiao@gmail.com>
2021-04-26 18:13:41 -0700

    initial commit

---
+++ firefox/user.js
@@ -0,0 +1,5 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("general.config.filename", "mozilla.cfg");
+user_pref("general.config.obscure_value", 0);
+user_pref("general.config.sandbox_enabled", false);
